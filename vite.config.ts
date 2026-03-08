bf49106426c160590750854054622151090e731c
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2026-03-08 13:24:16 +0500

    fix: build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/8-march-family/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
